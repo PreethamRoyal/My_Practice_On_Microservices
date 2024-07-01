e20c0b5bb829a737bef4cc27fcc975248ef0bfd4
preetham.sb700 <preetham.sb700@gmail.com>
2024-07-02 00:23:40 +0530

    committed on 02/07/2024

---
+++ employee-service/employee-service/src/main/java/net/javaguides/employeeservice/service/Impl/EmployeeServiceImpl.java
@@ -0,0 +1,49 @@
+package net.javaguides.employeeservice.service.Impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.employeeservice.dto.EmployeeDto;
+import net.javaguides.employeeservice.entity.Employee;
+import net.javaguides.employeeservice.repository.EmployeeRepository;
+import net.javaguides.employeeservice.service.EmployeeService;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+    @Override
+    public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
+
+        Employee employee=new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+        Employee savedEmployee=employeeRepository.save(employee);
+
+        EmployeeDto savedEmployeeDto=new EmployeeDto(
+                savedEmployee.getId(),
+                savedEmployee.getFirstName(),
+                savedEmployee.getLastName(),
+                savedEmployee.getEmail()
+        );
+        return savedEmployeeDto;
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+
+        Employee employee=employeeRepository.findById(employeeId).get();
+
+        EmployeeDto employeeDto=new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+        return employeeDto;
+    }
+}
